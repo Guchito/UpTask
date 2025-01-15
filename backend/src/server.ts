3f7a72007764e9fdd46e9e143dd79bf5433a77f2
guchito <a_gaggero@hotmail.com>
2025-01-15 01:48:32 +0100

    router created. post not working

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db';
-
+import projectRoutes from './routes/projectRoutes';
 
 dotenv.config();
 
@@ -9,5 +9,9 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
+//Routes
+app.use('/api/projects', projectRoutes);
 
 export default app;
