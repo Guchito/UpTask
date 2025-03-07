8ef3088f31a4b00d36720c6f3032bf726e341a9a
guchito <a_gaggero@hotmail.com>
2025-03-08 00:49:55 +0100

    Edit the form from the frontend

--- frontend/src/components/tasks/TaskCard.tsx
+++ frontend/src/components/tasks/TaskCard.tsx
@@ -2,11 +2,14 @@ import { Fragment } from "react"
 import { Menu, Transition } from "@headlessui/react"
 import { EllipsisVerticalIcon } from "@heroicons/react/20/solid"
 import { Task } from "@/types"
+import { useNavigate } from "react-router-dom"
 
 type TaskCardProps = {
   task: Task
 }
 export default function TaskCard({task} : TaskCardProps) {
+    const navigate = useNavigate()
+
   return (
     <li className="p-5 bg-white border-slate-300 flex justify-between gap-3">
         <div className="min-w-0 flex flex-col gap-y-4">
@@ -33,7 +36,10 @@ export default function TaskCard({task} : TaskCardProps) {
                             </button>
                         </Menu.Item>
                         <Menu.Item>
-                            <button type='button' className='block px-3 py-1 text-sm leading-6 text-gray-900'>
+                            <button 
+                            type='button' 
+                            className='block px-3 py-1 text-sm leading-6 text-gray-900'
+                            onClick={() => navigate(location.pathname + `?editTask=${task._id}`)}>
                                 Edit Task
                             </button>
                         </Menu.Item>
