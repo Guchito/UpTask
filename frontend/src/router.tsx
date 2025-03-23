4583474e928d90610399cf8631e62a19115a5ebc
guchito <a_gaggero@hotmail.com>
2025-03-23 18:57:23 +0100

    Created the forms to login and sign up

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -4,6 +4,9 @@ import DashboardView from '@/views/DashboardView'
 import CreateProjectViews from './views/projects/CreateProjectViews'
 import EditProjectView from './views/projects/EditProjectView'
 import ProjectDetailsView from './views/projects/ProjectDetailsView'
+import AuthLayout from './layouts/AuthLayout'
+import LoginView from './views/auth/LoginView'
+import RegisterView from './views/auth/RegisterView'
 
 export default function Router(){
 
@@ -17,6 +20,11 @@ export default function Router(){
                     <Route path="/projects/:projectId/" element={<ProjectDetailsView />} />
                     <Route path="/projects/:projectId/edit" element={<EditProjectView />} />
                 </Route>
+                <Route element={<AuthLayout />}>
+                    <Route path="/auth/login" element={<LoginView />}/>
+                    <Route path='/auth/signup' element={<RegisterView />}/>
+                </Route>
+
             </Routes>
         </BrowserRouter>
     )
